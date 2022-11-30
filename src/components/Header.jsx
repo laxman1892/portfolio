5d026a6c260df270b65d3a21a1758e5c94823209
laxman1892 <lxmnrzl@gmail.com>
2022-11-30 09:21:15 +0545

    first-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return <div className="container">
+      <h1> Laxman </h1>
+      <nav>
+         <ul>
+           <li>Home</li>
+           <li>Projects</li>
+           <li>Contact Me</li>
+         </ul>
+      </nav>
+  </div>
+}
+
+export default Header;
