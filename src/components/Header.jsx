28ca2f45ec03c9f6d81c39379dfabf713309328e
laxman1892 <lxmnrzl@gmail.com>
2022-12-01 21:53:46 +0545

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './styles.css';
 
- 
+
 
 function Header() {
-  return (<div className="container">
+    return (<div className="container">
       <h1> Laxman </h1>
       <nav>
         <ul>
           <li> <a href=""> Home </a></li>
           <li> <a href=""> Projects </a></li>
           <li> <a href=""> Contact </a></li>
-        </ul>
-      </nav>
-  </div>);
+    </ul>
+    </nav>
+    </div>
+    );
 }
 
 
